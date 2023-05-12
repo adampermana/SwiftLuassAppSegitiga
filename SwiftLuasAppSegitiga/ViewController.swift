d8c38e1ab36668dc898a45f9e4f4985da16f2714
Adam Permana <adampermana111@gmail.com>
2023-05-11 23:36:25 -0700

    Initial Commit

---
+++ SwiftLuasAppSegitiga/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  SwiftLuasAppSegitiga
+//
+//  Created by Adam Permana on 11/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtAlas: UITextField!
+    
+    
+    @IBOutlet weak var txtTinggi: UITextField!
+    
+    
+    @IBAction func buttonHitung(_ sender: UIButton) {
+        
+        let alas = txtAlas.text!
+                let tinggi = txtTinggi.text!
+
+                let floatAlas = Float(alas)!
+                let floatTinggi = Float(tinggi)!
+
+                let hasil = 0.5 * floatAlas * floatTinggi
+
+                txtHasil.text = String(hasil)
+
+            }
+        
+        
+        
+    @IBOutlet weak var txtHasil: UILabel!
+    
+
+
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
